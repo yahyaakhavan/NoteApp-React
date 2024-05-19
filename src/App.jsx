c491f9196771b23e5bef24ac66976d0eab9bc970
YAHYA <yahya.akhavan@gmail.com>
2024-05-19 09:54:49 +0330

    some styles changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import "./App.css";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div className="note-app">
+      <div className="note-form">
+        <h3>Add New Note</h3>
+        <input className="text-field" type="text" placeholder="Note Title..." />
+        <input
+          className="text-field"
+          type="text"
+          placeholder="Note Description..."
+        />
+        <button className="btn btn--primary">Add New Note</button>
+      </div>
+      <div className="note-container">
+        <p>No Notes has already been added!</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import SideBar from "./Sidebar.jsx";
+
 import Header from "./Header.jsx";
 import "./index.css";
 
@@ -9,6 +9,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <App />
-    <SideBar />
   </React.StrictMode>
 );
