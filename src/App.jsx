68d61e36eb56d45652391edb1795a84cec1505b9
YAHYA <yahya.akhavan@gmail.com>
2024-05-24 22:49:36 +0330

    Add onDelete and onComplete function

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,29 @@ function App() {
   const handleAddNote = (newNote) => {
     setNotes((prevNotes) => [...prevNotes, newNote]);
   };
+  const handleDeleteNote = (e) => {
+    let arr = [];
+    for (let i = 0; i < notes.length; i++) {
+      if (notes[i].id != e.target.dataset.noteid) {
+        arr.push(notes[i]);
+      }
+    }
+    setNotes(arr);
+  };
+  const handleCompletedNote = (e, id) => {
+    console.log(e.target.checked, id);
+  };
   return (
     <div className="container">
       <div className="note-header"></div>
       <div className="note-app">
         <AddNewNote onAddNote={handleAddNote} />
         <div className="note-container">
-          <NoteList notes={notes} />
+          <NoteList
+            notes={notes}
+            onDeleteNote={handleDeleteNote}
+            onCompleted={handleCompletedNote}
+          />
         </div>
       </div>
     </div>
